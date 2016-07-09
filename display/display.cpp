25034aa7816dd48dbe7843fa2b39d4b8ff963c87
Samuel Sikes <sammaniamsam@github.com>
2016-07-09 11:45:26 -0500

    updated display

--- display/display.cpp
+++ display/display.cpp
@@ -1,21 +1,37 @@
-/*******************************************************************
-* sensor.cpp
-* Programming Assignment 1 : Sensor Simulation
-* Author: Samuel Sikes
-* Date: June 2016
-* This program is entirely my own work
-*******************************************************************/
-
+//====================================================================
+// sensor.cpp
+// Programming Assignment 1 : Sensor Simulation
+// Author: Samuel Sikes
+// Date: June 2016
+// This program is entirely my own work
+//====================================================================
 #include "display.h"
 
-/***********************************/
+//---------------------------------
+//---------------------------------
+//--------PUBLIC METHODS----------
+//---------------------------------
+//---------------------------------
+
+//---------------------------------
+//function: display()
+//Dynamically creates displayNode vector
+//and sets vPtr to dynamically
+//created vector.
+//---------------------------------
 display::display(){
     vPtr = new std::vector<displayNode*>;
 };
-/***********************************/
+
+//---------------------------------
+//function: ~display()
+//Deletes dynamic vector and nodes,
+//sets private pointer to null,
+//sets vSensorNodePtrs to null.
+//---------------------------------
 display::~display(){
-    for(int i=0; i < vPtr->size(); i++) {
-        for(int j=0; j < vPtr->at(i)->vSensorNodePtrs.size(); j++)
+    for(unsigned long i=0; i < vPtr->size(); i++) {
+        for(unsigned long j=0; j < vPtr->at(i)->vSensorNodePtrs.size(); j++)
             vPtr->at(i)->vSensorNodePtrs.at(j) = NULL;
         delete vPtr->at(i);
         vPtr->at(i) = NULL;
@@ -23,16 +39,26 @@ display::~display(){
     delete vPtr;
     vPtr = NULL;
 };
-/***********************************/
+
+//---------------------------------
+//function: getDisplay()
+//Fills dynamically created displayNode
+//vector with displayNode ptrs.
+//---------------------------------
 void display::getDisplay(displayNode* dnPtr){
     vPtr->push_back(dnPtr);
 };
-/***********************************/
+
+//---------------------------------
+//function: displayData()
+//Displays all display devices and
+//their associated sensor(s).
+//---------------------------------
 void display::displayData(){
-    for(int i=0; i < vPtr->size(); i++){
+    for(unsigned long i=0; i < vPtr->size(); i++){
         std::cout << "Display Device: " << vPtr->at(i)->type
         << "\n";
-        for(int j=0; j < vPtr->at(i)->vSensorNodePtrs.size(); j++) {
+        for(unsigned long j=0; j < vPtr->at(i)->vSensorNodePtrs.size(); j++) {
             std::cout << std::setw(15) << "Sensor " <<
             vPtr->at(i)->vSensorNodePtrs.at(j)->ID << "\n"
             << std::setw(23) <<"Type = " <<
@@ -45,7 +71,14 @@ void display::displayData(){
         }
     }
 };
-/***********************************/
+
+//---------------------------------
+//function: relayDisplayData()
+//Returns the private pointer vPtr,
+//which is pointing to a vector
+//container containing ptrs to
+//display nodes.
+//---------------------------------
 std::vector<displayNode*>* display::relayDisplayData(){
     return vPtr;
 };
--- display/display.h
+++ display/display.h
@@ -1,57 +1,39 @@
-/*******************************************************************
-* display.h
-* Programming Assignment 1 : Sensor Simulation
-* Author: Samuel Sikes
-* Date: June 2016
-* This program is entirely my own work
-*******************************************************************/
+//====================================================================
+// display.h
+// Programming Assignment 1 : Sensor Simulation
+// Author: Samuel Sikes
+// Date: June 2016
+// This program is entirely my own work
+//====================================================================
 #include <iostream>
 #include <iomanip>
 #include <vector>
+//---------------------------------
 #include "../sensor/sensor.h"
+//---------------------------------
 
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+//---------------------------------
 struct displayNode {
     char type[64];
     int IDs[7];
     int IDCount;
     std::vector<sensorNode*> vSensorNodePtrs;
 };
+//---------------------------------
 
 class display {
 
 private:
-    std::vector<displayNode*> *vPtr;
+    std::vector<displayNode*> *vPtr; //ptr to vector of displayNode ptrs
 public:
-    /***********************************/
-    //dynamically create displayNode vector
-    //set vPtr to dynamically created vector
     display();
-
-    /***********************************/
-    //deletes dynamic vector and nodes,
-    //sets private pointer to null,
-    //sets vSensorNodePtrs to null
     ~display();
-
-    /***********************************/
-    //fills dynamically created displayNode
-    //vector with displayNode(s)
     void getDisplay(displayNode* dnPtr);
-
-    /***********************************/
-    //returns the private pointer vPtr,
-    //which is pointing to a vector
-    //container containing all the display
-    //nodes
-    std::vector<displayNode*>* relayDisplayData();
-
-    /***********************************/
-    //displays all display devices and
-    //their associated sensor(s)
     void displayData();
+    std::vector<displayNode*>* relayDisplayData();
 };
 
 #endif
