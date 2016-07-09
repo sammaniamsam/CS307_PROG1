a9af3d241c2ab6dc5e457d1f519fd9a66e1b9c62
Samuel Sikes <sammaniamsam@github.com>
2016-07-09 11:18:45 -0500

    updated sensor

--- sensor/sensor.cpp
+++ sensor/sensor.cpp
@@ -1,44 +1,90 @@
-/*******************************************************************
-* sensor.cpp
-* Programming Assignment 1 : Sensor Simulation
-* Author: Samuel Sikes
-* Date: June 2016
-* This program is entirely my own work
-*******************************************************************/
-
+//====================================================================
+// sensor.cpp
+// Programming Assignment 1 : Sensor Simulation
+// Author: Samuel Sikes
+// Date: June 2016
+// This program is entirely my own work
+//====================================================================
 #include "sensor.h"
 
-/***********************************/
+//---------------------------------
+//---------------------------------
+//--------PRIVATE METHODS----------
+//---------------------------------
+//---------------------------------
+
+//---------------------------------
+//function: generateSensorData()
+//Generates data for each sensor node
+//in the sensor node vector that
+//the private member variable vPtr
+//points to.
+//---------------------------------
+void sensor::generateSensorData(unsigned long& vSize) {
+    for(unsigned long i = 0; i < vSize; i++) {
+        srand((unsigned int) (time(NULL)));
+        vPtr->at(i)->sensorData = vPtr->at(i)->minVal + (rand() %
+                (int) (vPtr->at(i)->maxVal - vPtr->at(i)->minVal + 1));
+    }
+};
+
+//---------------------------------
+//---------------------------------
+//--------PUBLIC METHODS----------
+//---------------------------------
+//---------------------------------
+
+//---------------------------------
+//function: sensor()
+//Dynamically create sensorNode vector
+//set vPtr to dynamically created vector.
+//---------------------------------
 sensor::sensor(){
     vPtr = new std::vector<sensorNode*>;
 };
-/***********************************/
+
+//---------------------------------
+//function: ~sensor()
+//Deletes ptrs within dynamic vector
+//of sensor node ptrs and
+//deletes dynamic vector.
+//---------------------------------
 sensor::~sensor() {
-    for(int i=0; i < vPtr->size(); i++) {
+    for(unsigned int i=0; i < vPtr->size(); i++) {
         delete vPtr->at(i);
         vPtr->at(i) = NULL;
     }
     delete vPtr;
     vPtr = NULL;
 };
-/***********************************/
+
+//---------------------------------
+//function: getSensor()
+//Fills dynamically created sensorNode
+//vector with sensorNode(s).
+//---------------------------------
 void sensor::getSensor(sensorNode* snPtr){
     vPtr->push_back(snPtr);
 };
-/***********************************/
+
+//---------------------------------
+//funciton: updateSensors()
+//Calls fctn generateSensorData
+//and passes it the size of the vector
+//container containing sensor nodes.
+//---------------------------------
 void sensor::updateSensors() {
-    unsigned long size = (vPtr->size()) - 1;
-    generateSensorData(size);
+    unsigned long size = vPtr->size();
+    this->generateSensorData(size);
 };
-/***********************************/
-void sensor::generateSensorData(unsigned long vSize) {
-    srand((unsigned int)(time(NULL)));
-    vPtr->at(vSize)->sensorData = vPtr->at(vSize)->minVal + (rand() %
-            (int)(vPtr->at(vSize)->maxVal - vPtr->at(vSize)->minVal + 1));
-    if(vSize == 0) return;
-    generateSensorData(vSize-1);
-};
-/***********************************/
+
+//---------------------------------
+//funciton: relaySensorData()
+//Returns the private pointer vPtr,
+//which is pointing to a vector
+//container containing ptrs to all
+//the sensor nodes.
+//---------------------------------
 std::vector<sensorNode*>* sensor::relaySensorData(){
     return vPtr;
 };
--- sensor/sensor.h
+++ sensor/sensor.h
@@ -1,17 +1,19 @@
-/*******************************************************************
-* sensor.h
-* Programming Assignment 1 : Sensor Simulation
-* Author: Samuel Sikes
-* Date: June 2016
-* This program is entirely my own work
-*******************************************************************/
+//====================================================================
+// sensor.h
+// Programming Assignment 1 : Sensor Simulation
+// Author: Samuel Sikes
+// Date: June 2016
+// This program is entirely my own work
+//====================================================================
 #include <iostream>
 #include <vector>
 #include <time.h>
+//---------------------------------
 
 #ifndef SENSOR_H
 #define SENSOR_H
 
+//---------------------------------
 struct sensorNode {
     char type[64];
     char material[64];
@@ -21,48 +23,20 @@ struct sensorNode {
     double maxVal;
     double sensorData;
 };
+//---------------------------------
 
 class sensor {
 
 private:
-    /***********************************/
-    //vector pointer to vector
-    //of sensorNode pointers
-    std::vector<sensorNode*> *vPtr;
+    std::vector<sensorNode*> *vPtr; //vector ptr to vector of sensorNode ptrs
 
-
-    /***********************************/
-    //A recursive function that generates data
-    //for each sensor node in a vector container
-    void generateSensorData(unsigned long vSize);
+    void generateSensorData(unsigned long& vSize);
 
 public:
-    /***********************************/
-    //dynamically create sensorNode vector
-    //set vPtr to dynamically created vector
     sensor();
-
-    /***********************************/
-    //deletes dynamic vector and nodes
-    //set private pointer to null
     ~sensor();
-
-    /***********************************/
-    //fills dynamically created sensorNode
-    //vector with sensorNode(s)
     void getSensor(sensorNode* snPtr);
-
-    /***********************************/
-    //calls the recursive fctn generateSensorData
-    //and passes it the size of the vector
-    //container containing sensor nodes
     void updateSensors();
-
-    /***********************************/
-    //returns the private pointer vPtr,
-    //which is pointing to a vector
-    //container containing all the sensor
-    //nodes
     std::vector<sensorNode*>* relaySensorData();
 };
 
